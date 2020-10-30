3cf4c31d21625d1b772dea9f05422048fcd4e1b4
Catherine Liahn Yang <liahnee@gmail.com>
2020-10-30 05:12:51 -0700

    pull question function changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './stylesheets/App.css';
 
 import allQuestions from './Apprentice_TandemFor400_Data.json';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Question from './components/question';
 import Answer from './components/answers';
 import Score from './components/score';
 
 function App() {
-
 	const [ score, setScore ] = useState(0);
-	const [ questionList, setquestionList ] = useState([...allQuestions]);
+	const [ questionList, setQuestionList ] = useState(new Array());
 	const [ question, setQuestion ] = useState(null);
 	const [ answer, setAnswer ] = useState(null);
 	const [ answerChoices, setAnswerChoices ] = useState(new Array());
@@ -21,12 +20,33 @@ function App() {
 
 	const [ questionCount, setQuestionCount ] = useState(0);
 
+	useEffect(() => {
+		createQuestionList();
+	}, []);
+
+	const createQuestionList = () => {
+		const max = allQuestions.length - 1;
+		const tempIdx = [];
+		const tempList = [];
+		let count = 0;
+		while (count <= 10) {
+			let num = randomInt(max);
+			if (tempIdx.includes(num)) {
+				continue;
+			} else {
+				tempIdx.push(num);
+				const newQ = allQuestions[num];
+				newQ.selectedAnswer = null;
+				tempList.push(newQ);
+				count++;
+			}
+		}
+		setQuestionList(tempList);
+		return;
+	};
+
 	const pullQuestion = () => {
-		const max = questionList.length - 1;
-    const pulledQuestion = questionList.splice(randomInt(max), 1)[0];
-    console.log('pulledQuestion', pulledQuestion)
-    console.log('allQuestions', allQuestions);
-    console.log("questioncount", questionCount);
+		const pulledQuestion = questionList[questionCount];
 		setQuestion(pulledQuestion.question);
 		setAnswer(pulledQuestion.correct);
 
@@ -34,6 +54,7 @@ function App() {
 		randAnswerChoices.splice(randomInt(4), 0, pulledQuestion.correct);
 		setAnswerChoices(randAnswerChoices);
 
+		setQuestionCount(questionCount + 1);
 		return;
 	};
 
@@ -41,7 +62,7 @@ function App() {
 		pullQuestion();
 		setShowAnswerButton(true);
 		setRevealAnswer(false);
-		countQuestion();
+		setSelectedAnswer(null);
 		return;
 	};
 
@@ -52,38 +73,35 @@ function App() {
 		}
 		setShowAnswerButton(false);
 		setRevealAnswer(true);
+		questionList[questionCount - 1].selectedAnswer = selectedAnswer;
 		return;
 	};
 
 	const selectAnswer = (choice) => {
 		if (!revealAnswer) {
+			// cannot change answer while answer is being shown
 			setSelectedAnswer(choice);
 		}
 		return;
 	};
 
-	const countQuestion = () => {
-		setQuestionCount(questionCount + 1);
-		return;
-	};
-
 	const reset = () => {
 		setQuestionCount(0);
-    setquestionList([...allQuestions]);
-    setQuestion(null);
-    setAnswerChoices(new Array);
-    setAnswer(null);
-    setScore(0);
-    setSelectedAnswer(null);
-    setShowAnswerButton(true);
-    setRevealAnswer(false);
+		setQuestionList([ ...allQuestions ]);
+		setQuestion(null);
+		setAnswerChoices(new Array());
+		setAnswer(null);
+		setScore(0);
+		setSelectedAnswer(null);
+		setShowAnswerButton(true);
+		setRevealAnswer(false);
 
 		return;
 	};
 
 	return (
 		<div className="App">
-			{questionCount <= 10 ?(
+			{questionCount <= 10 ? (
 				<div>
 					<Score score={score} />
 					{showAnswerButton ? (
@@ -91,18 +109,21 @@ function App() {
 					) : (
 						<button onClick={nextQuestion}> Next Question</button>
 					)}
-
-					<Question text={question} count={questionCount} />
-					{answerChoices.map((choice, i) => (
-						<Answer
-							key={i}
-							text={choice}
-							select={selectAnswer}
-							selected={selectedAnswer == choice ? true : false}
-							reveal={revealAnswer}
-							answer={answer}
-						/>
-					))}
+					<div className="question-container">
+						<Question text={question} count={questionCount} />
+						<div className="answers-container">
+							{answerChoices.map((choice, i) => (
+								<Answer
+									key={i}
+									text={choice}
+									select={selectAnswer}
+									selected={selectedAnswer == choice ? true : false}
+									reveal={revealAnswer}
+									answer={answer}
+								/>
+							))}
+						</div>
+					</div>
 				</div>
 			) : (
 				<div>
@@ -114,8 +135,8 @@ function App() {
 	);
 }
 
-export default App;
-
 const randomInt = (max) => {
 	return Math.floor(Math.random() * Math.floor(max));
 };
+
+export default App;
